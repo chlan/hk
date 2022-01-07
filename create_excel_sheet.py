444dbaf84dd69bd2f8da71d3037d07591d7a2e2d
Christopher Lange <clange@gmx.net>
2022-01-07 23:34:40 +0100

    Added creation of the fragen excel sheets

--- create_excel_sheet.py
+++ create_excel_sheet.py
@@ -2,8 +2,6 @@
 
 import openpyxl as xl
 
-wb= xl.load_workbook('template_bewertung_20191004.xlsx')
-
 pl = [['Sasse', 'Jonas'],
       ['Sayman', 'Savas'],
       ['Schliemann', 'Tobias'],
@@ -20,11 +18,17 @@ for r in range(0, len(pl)):
     n1= pl[r][0] + '_' + pl[r][1]
     fn1= 'bewertung_' + n1 + '_CHL' + '.xlsx'
     fn2= 'bewertung_' + n1 + '.xlsx'
+    fn3= 'fragen_' + n1 + '.xlsx'
     print(r,n0, n1, fn1, fn2)
-    wb= xl.load_workbook('template_bewertung_20191004.xlsx')
-    sheet= wb['Dokumentation']
+    wb1= xl.load_workbook('template_bewertung.xlsx')
+    wb2= xl.load_workbook('template_fragen.xlsx')
+    sheet= wb1['Dokumentation']
     cell= sheet.cell(7,2)
     cell.value= n0
-    wb.save(fn1)
-    wb.save(fn2)
+    wb1.save(fn1)
+    wb1.save(fn2)
+    sheet= wb2['Tabelle1']
+    cell= sheet.cell(1,3)
+    cell.value= n0
+    wb2.save(fn3)
 
